c2b2eea7fbba79920f2df2ed40a5594a8194b89a
juan <142954292+charsplusjk@users.noreply.github.com>
2023-09-02 13:31:19 -0300

    add: 8XY0 - VX = VY

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -156,11 +156,18 @@ impl Emu {
                 let nn = (op & 0xFF) as u8;
                 self.v_reg[x] = nn;
             },
+            // VX += NN
             (7, _, _, _) => {
                 let x = digit2 as usize;
                 let nn = (op &0xFF) as u8;
                 self.v_reg[x] = self.v_reg[x].wrapping_add(nn);
             },
+            // VX = VY
+            (8, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] = self.v_reg[y];
+            }
             (_, _, _, _) => unimplemented!("Uninplemented opcode: {}", op),
         }
     }
